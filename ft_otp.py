1ec83b715828cf1128163655f94b2b5460c9bccd
Andres Solano rueda <asolano-@c1r10s1.42malaga.com>
2022-08-26 10:55:09 +0200

    a medias

--- ft_otp.py
+++ ft_otp.py
@@ -30,4 +30,103 @@ def leer_argumentos():
 def iniciar_analizador():
     # Analizador de argumentos de la línea de comandos.
     analizador = argparse.ArgumentParser(
+        description="Herramienta casera para generar constraseñas TOTP.",
+        epilog="Ejercicio 'ft_otp' del Bootcamp de Ciberseguridad de 42 Málaga.",
+        )
 
+    #Agregar las opciones del comando.
+    analizador.add_argument(
+            "-g",
+            metavar="fichero",
+            help="almacena una clave hexadecimal de 64 caracteres mínimo en un fichero 'ft_otp.key'.",
+            type=str)
+    analizador.add_argument(
+            "-k",
+            metavar="fichero",
+            help="genera una contraseña temporal usando un fichero y la muestra por pantalla.",
+            type=str)
+    analizador.add_argument(
+            "-qr",
+            metavar="fichero",
+            help="muestra un QR con la clave secreta.",
+            type=str)
+
+    # Obtener los argumentos de la línea de comandos.
+    return analizar.parse_args()
+
+
+#Verifica que un fichero contiene una clave que cumple los requisitos mínimos.
+def validar_fichero(fichero):
+    global semilla
+
+    # El fichero existe y es legible.
+    if not (os.path.isfile(fichero) or os.access(fichero, os.R_OK)):
+        print("Error: El fichero no existe o no es legible.")
+
+        return False
+
+    #Extraer clave del interior del fichero
+    with open(fichero, "r") as f:
+        clave = f.read()
+
+    # Verifica que la clave es hexadecimal y mide al menos 64 caracteres.
+    if not re.match(r'^[0-0a-fA-F]{64,}$', semilla):
+        print("La clave no es hexadecimal o tiene menos de 64 caracteres.")
+
+        """
+
+        Expresión regular:
+        ^           : límite inicial de la acotación de la cadena.
+        [0-9a-fA-F] : cualquier caracter hexadecimal (números, o letras desde 'a' hasta 'f' o desde 'A' hasta 'F').
+        {64,}       : cuantificador que indica longitud de 64 hasta ilimitados caracteres.
+        $           : límite final de la acotación de la cadena.
+
+        """
+
+        return False
+
+    return True
+
+
+# Generar un código temporal usando una clave secreta hexadecimal (semilla).
+# - secreto: clave hexadecimal secreta de la que extraer un OTP.
+def generar_OTP(semilla)
+
+    # Codificar la clave hexadecimal en una cadena de bytes.
+    clave_b = bytes.fromhex(semilla)
+
+    # Obtener y truncar el tiempo actual a una "ventana" de 30 segundos.
+    tiempo = int(time.time() // 30)
+
+    # Codificar el tiempo en una cadena de bytes.
+    timepo_b = struct.pack("<Q", tiempo)
+
+    # Generar el hash de la clave secreta (como cadena de bytes).
+    hash_b = hmac.digest(clave_b, tiempo_b, hashlib.sha1)
+
+    # Obtener el offset.
+    offset = hash_b[19] & 15 # Operacion AND entre '0b????' y '0b1111'.
+
+    # Generar el código
+    codigo = struct.unpac('<I', hash_b[offset:offset + 4])[0] # '[0]' porque 'struct.unpack' devuelve una lista.
+    codigo = (codigo & 0x7FFFFFFF) % 1000000
+
+    """
+
+    Generar un Valor HTOP (en este caso de 6 dígitos).
+    1. Generar un valor HMAC-SHA1.
+        -   Usando el valor de la clave y el valor del tiempo actual.
+        -   Será un 'str' de 20 bytes.
+    2. Generar un 'str' de 4 bytes ("Truncamiento Dinámico").
+        -   Usando el valor HMAC-SHA1 anterior.
+        -   Será un 'str' de 4 bytes a partir del byte 'hash[offset]'.
+    3. Computar el Valor HTOP.
+        -   Convertir el 'str' anterior a un entero.
+        -   Aplicarle módulo 10^'d', siendo 'd' la cantidad de dígitos (en este caso d = 6).
+
+    * Extraído de la sección 5.3 del RFC 4226.
+
+    """
+
+    # Devolver el código como 'str'.
+    return "{:06d}".format(codigo)
